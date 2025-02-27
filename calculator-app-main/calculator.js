af7cbde8b69e2eea002945556d4f8708039103fa
Ali_Jn <141764803+alijenzri@users.noreply.github.com>
2025-02-27 16:37:47 +0100

    Add files via upload

---
+++ calculator-app-main/calculator.js
@@ -0,0 +1,89 @@
+const calculatorScreen = document.querySelector('#screen');
+let currentInput = '';
+let currentOperation = '';
+let lastResult = '';
+
+function updateScreen() {
+    calculatorScreen.innerHTML = currentInput || lastResult || ' ';
+}
+
+function handleOperand(operand) {
+    if (lastResult !== '' && currentInput === '') {
+        lastResult = '';
+    }
+    currentInput += operand;
+    updateScreen();
+}
+
+function handleOperator(operator) {
+    if (currentInput === '' && lastResult === '') {
+        return;
+    }
+
+    if (currentInput !== '') {
+        currentOperation += currentInput + ' ' + operator;
+    } else {
+        currentOperation = lastResult + ' ' + operator;
+    }
+    currentInput = '';
+    lastResult = '';
+    console.log(`Current Operation: ${currentOperation}`);
+    updateScreen();
+}
+
+function handleEquals() {
+    if (currentInput === '' && currentOperation === '') {
+        return;
+    }
+
+    if (currentInput !== '') {
+        currentOperation += currentInput;
+    }
+    try {
+        const result = eval(currentOperation);
+        calculatorScreen.textContent = result;
+        lastResult = '' + result;
+        currentOperation = '';
+        currentInput = '';
+        console.log(`Result: ${result}`);
+    } catch (error) {
+        lastResult = '';
+        currentOperation = '';
+        currentInput = '';
+        updateScreen()
+    }
+}
+
+function handleDelete() {
+    if (currentInput == '' && lastResult != '') {
+        lastResult = lastResult.slice(0, -1);
+    } else {
+        currentInput = currentInput.slice(0, -1);
+    }
+    updateScreen();
+}
+
+function handleClear() {
+    currentInput = '';
+    currentOperation = '';
+    lastResult = '';
+    updateScreen();
+}
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.hasAttribute('data-operand')) {
+            handleOperand(button.getAttribute('data-operand'));
+        } else if (button.hasAttribute('data-operator')) {
+            handleOperator(button.getAttribute('data-operator'));
+        } else if (button.hasAttribute('data-equals')) {
+            handleEquals();
+        } else if (button.hasAttribute('data-delete')) {
+            handleDelete();
+        } else if (button.hasAttribute('data-clear')) {
+            handleClear();
+        }
+    });
+});
+
+updateScreen();
